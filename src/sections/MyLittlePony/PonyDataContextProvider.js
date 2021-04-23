cb129780153637c126bb1c6da7c5b5872cb32866
Poonam Thorup <poonam.thorup@gmail.com>
2021-04-23 22:47:22 +0200

    change back to real api

--- src/sections/MyLittlePony/PonyDataContextProvider.js
+++ src/sections/MyLittlePony/PonyDataContextProvider.js
@@ -1,22 +1,18 @@
 import React, { createContext } from 'react'
-import { data } from './my-little-pony-cat.json'
-// import useFetchData from '../../hooks/useFetchData'
+// import { data } from './my-little-pony-cat.json'
+import useFetchData from '../../hooks/useFetchData'
 
 const PonyDataContext = createContext()
 
 const PonyDataContextProvider = (props) => {
-    // let [loading, response, error] = useFetchData(
-    //     // 'https://jsonplaceholder.typicode.com/posts/',
-    //     // 'https://ponyweb.ml/v1/character/all?limit=10',
-    // )
+    let [loading, response, error] = useFetchData(
+        'https://ponyweb.ml/v1/character/all?limit=600',
+    )
 
-    const loading = false
-    const error = null
-
-    //console.log(data)
     return (
-        // remember it is ponyData:response.data
-        <PonyDataContext.Provider value={{ ponyData: data, loading, error }}>
+        <PonyDataContext.Provider
+            value={{ ponyData: response.data, loading, error }}
+        >
             {props.children}
         </PonyDataContext.Provider>
     )
